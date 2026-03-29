c1c124f0e27ecd3dcd201fc5eb45a849e91e1094
created-by-the-creator <25-00261@g.batstate-u.edu.ph>
2026-03-29 23:49:57 +0800

    Added some assignments/comments

--- GasQueue.cpp
+++ GasQueue.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main () {
 
     cout << "Official file draft for GasQueue: Fuel Distribution System.";
+    //Stacks : Assigned : Venice Anne Manacop
+    //Queue : Assigned: Kaixer Alvar
+    //Priority Queue : Assigned : Joshua Zamora
 
     return 0;
 }
