d287c7edaf380a5c7ce924d513d32fb380e36755
Kaixer Alvar <alvarkaixereoa@gmail.com>
2026-03-30 00:37:53 +0800

    added design

--- QueueBits.cpp
+++ QueueBits.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main (){
+    cout << "=========================================" << endl;
+    cout << "Yo wassup we are the QueueBits\n";
+    cout << "HELP\n";
+    cout << "PLEASE SEND HELP\n";
+    cout << "=========================================" << endl;
 
-    cout << "Yo wassup we are the QueueBits";
-    cout << "HELP";
-    cout << "PLEASE SEND HELP";
-
+    
 
     return 0;
 }
