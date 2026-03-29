e48e4d4b401a582211765e646742b313250d2bb1
Kaixer Alvar <alvarkaixereoa@gmail.com>
2026-03-30 00:39:31 +0800

    fixed error under GasQueue.cpp

--- GasQueue.cpp
+++ GasQueue.cpp
@@ -5,7 +5,7 @@ int main () {
 
     cout << "Official file draft for GasQueue: Fuel Distribution System.";
     //Stacks : Assigned : Venice Anne Manacop
-    cout << "Stacks assigned to Venice Anne"
+    cout << "Stacks assigned to Venice Anne";
     //Queue : Assigned: Kaixer Alvar
     //Priority Queue : Assigned : Joshua Zamora
 
